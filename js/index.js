7e5e1f6647cb5cf4d3ab5cfed4f8490bf94636e6
IngenieroJosser <133165583+IngenieroJosser@users.noreply.github.com>
2023-10-22 21:58:55 -0700

    First Commit

---
+++ js/index.js
@@ -0,0 +1,28 @@
+// document.addEventListener("DOMContentLoaded", function () {
+//     const menuToggle = document.getElementById("menuToggle");
+//     const menu = document.querySelector(".menu");
+
+//     menuToggle.addEventListener("click", function () {
+//         if (menu.style.left === "" || menu.style.left === "90px") {
+//             menu.style.left = "0";
+//         } else {
+//             menu.style.top = "90px";
+//         }
+//     });
+// });
+
+
+
+    // const menuToggle = document.getElementById("menuToggle"); // Obtén el elemento de la hamburguesa
+    // const menu = document.querySelector(".menu"); // Obtén el elemento del menú
+
+    // // Agrega un evento de clic al icono de hamburguesa
+    // menuToggle.addEventListener("click", function () {
+    //     // Si el menú está oculto o fuera de la pantalla, muéstralo
+    //     if (menu.style.left === "" || menu.style.left === "-250px") {
+    //         menu.style.left = "0";
+    //     } else {
+    //         // Si el menú está visible, ocúltalo
+    //         menu.style.left = "-250px";
+    //     }
+    // });
